efaeb8a0ebaa6f9593b1a649c7854d168fafc649
hotate29 <hotate_oc@yahoo.co.jp>
2024-03-08 19:28:08 +0000

    fixup! とりあえず動くvite
    ちょっとした

--- atcoder-problems-frontend/src/components/NavigationBar.tsx
+++ atcoder-problems-frontend/src/components/NavigationBar.tsx
@@ -34,7 +34,7 @@ export const NavigationBar = () => {
     <div className={isNavigationFixed ? "sticky-top" : ""}>
       <Navbar color="dark" dark expand="lg">
         <NavbarBrand tag={RouterLink} to="/" className="mb-0 h1">
-          AtCoder Problems aaaaaaaaaaaa
+          AtCoder Problems
         </NavbarBrand>
 
         <NavbarToggler onClick={(): void => setIsOpen(!isOpen)} />
